9c222312f94ab3c819a98db1fe0bfa7eb894fc4b
AKToran <aktoran60@gmail.com>
2025-06-30 16:57:40 +0600

    sending firebase token to server

--- src/api/ApplicationsApi.js
+++ src/api/ApplicationsApi.js
@@ -1,4 +1,8 @@
-export const myApplicationsPromise = (email) =>{
-  return fetch(`http://localhost:3000/applications?email=${email}`)
+export const myApplicationsPromise = (email, token) =>{
+  return fetch(`http://localhost:3000/applications?email=${email}`,{
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  })
   .then(res => res.json());
 }
