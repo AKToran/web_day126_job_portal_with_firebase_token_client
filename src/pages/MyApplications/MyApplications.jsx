b92fd435d738527c71e44899dc7d5d4f7428e776
AKToran <aktoran60@gmail.com>
2025-07-04 20:05:23 +0600

    custom hook for api call that uses the interceptor hook

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,16 +1,19 @@
 import React, { Suspense } from 'react';
 import ApplicationList from './ApplicationList';
 import UseAuths from '../../hooks/UseAuths';
-import { myApplicationsPromise } from '../../api/ApplicationsApi';
+import useApplicationApi from '../../api/useApplicationApi';
+// import { myApplicationsPromise } from '../../api/ApplicationsApi';
+
 
 const MyApplications = () => {
   const { user } = UseAuths();
+  const { myApplicationsPromise } = useApplicationApi();
 
   return (
     <div>
       <h2>All my Applications:</h2>
       <Suspense fallback={<p>loading applications......................</p>}>
-        <ApplicationList myApplicationsPromise={myApplicationsPromise(user.email, user.accessToken)} />
+        <ApplicationList myApplicationsPromise={myApplicationsPromise(user.email)} />
       </Suspense>
     </div>
   );
